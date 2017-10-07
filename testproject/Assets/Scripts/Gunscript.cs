ffbc3e1a79efa0c244df7e613855dcce214a4ddb
Haziis <32591443+Haziis@users.noreply.github.com>
2017-10-07 17:33:48 +0300

    0.0.1 alpha
    currently only some graphics are working
    
    Added running anim
    added aim
    added jump anim (Not scripted yet)
    added simple weapon
    added test map
    
                     Upcoming!
    more maps
    game mode
    shooting
    more weapons
    and more!

---
+++ testproject/Assets/Scripts/Gunscript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gunscript : MonoBehaviour {
+
+	public Animator animator;
+	private bool Isaiming = false;
+	public Image crosshair;
+
+	public float range = 100f;
+	public float damage = 10f;
+	public Camera fpscam;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//running
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			animator.SetBool("IsRunning", true);
+		}
+		if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			animator.SetBool ("IsRunning", false);
+		}
+		//aiming
+		if (Input.GetButtonDown("Fire2"))
+			{
+			Isaiming = !Isaiming;
+			animator.SetBool ("IsAiming", Isaiming);
+			crosshair.enabled = !Isaiming;
+
+
+
+			}
+		//Shooting
+		if (Input.GetButtonDown ("Fire1")) {
+			Shoot ();
+		}
+
+	}
+	void Shoot ()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast (fpscam.transform.position, fpscam.transform.forward, out hit, range)) {
+			Debug.Log(hit.transform.name + "Has been hit!");
+		}
+	}
+}
